55788d6ef0b05c9a2233b0b74bbce5ed17109033
benji011 <wolfnun011@gmail.com>
2021-03-29 11:30:19 +0900

    fix: added login_required for all views

--- personalprofile/urls.py
+++ personalprofile/urls.py
@@ -1,13 +1,15 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 from . import views
 
 app_name = 'personalprofile'
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('add/', views.AddView.as_view(), name='add'),
-    path('profiles/', views.AllProfilesView.as_view(), name='profiles'),
-    path('<int:pk>/', views.InfoView.as_view(), name='info'),
-    path('edit/<int:pk>/', views.EditView.as_view(), name='edit'),
-    path('delete/<int:pk>/', views.DeleteView.as_view(), name='delete')
+    path('', views.home, name='index'),
+    path('accounts/profile/', login_required(views.home, login_url='/login/'), name='profile'),
+    path('add/', login_required(views.AddView.as_view(), login_url='/login/'), name='add'),
+    path('profiles/', login_required(views.AllProfilesView.as_view(), login_url='/login/'), name='profiles'),
+    path('<int:pk>/', login_required(views.InfoView.as_view(), login_url='/login/'), name='info'),
+    path('edit/<int:pk>/', login_required(views.EditView.as_view(), login_url='/login/'), name='edit'),
+    path('delete/<int:pk>/', login_required(views.DeleteView.as_view(), login_url='/login/'), name='delete')
 ]
